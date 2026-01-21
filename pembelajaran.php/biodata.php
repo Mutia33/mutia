b8bee7fb4757c8500dfd361e4a5656ae6a6b2b30
mutia33 <rahmamutia516@gmail.com>
2026-01-21 09:46:17 +0700

    update pembelajaran

--- pembelajaran.php/biodata.php
+++ pembelajaran.php/biodata.php
@@ -1,3 +1,6 @@
 <?php
-echo "Nama : Mutia <br>" ;
-echo "Kelas : XI TJKT 2" ;
+echo "Nama : Mutia Rahma Rismawan <br>" ;
+echo "Kelas : XI TJKT 2 <br>" ;
+echo "No Absen : 23 <br>" ;
+echo "Sekolah : SMKN 2 Baleendah <br>" ;
+?>
